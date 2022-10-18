d5495a8b931eeb721eecca342ba08677ef053fc6
dagibb995 <116054555+dagibb995@users.noreply.github.com>
2022-10-17 22:44:14 -0600

    Create start.js

---
+++ start.js
@@ -0,0 +1,11 @@
+export async function main(ns) {
+  if (ns.getHostname() !== "home") {
+    throw new Exception("Run the script from home");
+  }
+
+  await ns.wget(
+    `https://raw.githubusercontent.com/dagibb995/BitBurner/new/main/initHacking.js?ts=${new Date().getTime()}`,
+    "initHacking.js"
+  );
+  ns.spawn("initHacking.js", 1);
+}
